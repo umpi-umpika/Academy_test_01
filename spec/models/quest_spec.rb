ce83cc6493fb612dfdd250454be425d5bbc6cebc
umpi-umpika <m.umpika@outlook.com>
2025-06-07 14:39:03 +0700

    refactor: Lint

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Quest, type: :model do
   describe 'attributes' do
     it 'has name and status attributes' do
-      quest = Quest.new(name:'Test Quest', status:false)
+      quest = Quest.new(name: 'Test Quest', status: false)
 
       expect(quest.name).to eq('Test Quest')
       expect(quest.status).to eq(false)
